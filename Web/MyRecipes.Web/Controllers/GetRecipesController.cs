0b6964c81e8e2da8325baff4ee3db09c4d8540a2
Martin Zdravkov <96056067+MartinZ17@users.noreply.github.com>
2024-03-10 14:43:03 +0200

    Update GetRecipesController.cs

--- Web/MyRecipes.Web/Controllers/GetRecipesController.cs
+++ Web/MyRecipes.Web/Controllers/GetRecipesController.cs
@@ -1,9 +1,5 @@
 ﻿namespace MyRecipes.Web.Controllers
 {
-    using System.Threading.Tasks;
-
-    using Microsoft.AspNetCore.Mvc;
-    using MoiteRecepti.Services;
 
     public class GetRecipesController : BaseController
     {
